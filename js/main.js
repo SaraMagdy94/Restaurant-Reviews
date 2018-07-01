f7d95695f8a1c62594824cf88d0f371d89d9a5b1
SaraMagdy94 <30289008+SaraMagdy94@users.noreply.github.com>
2018-07-02 02:55:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -216,19 +216,12 @@ addMarkersToMap = (restaurants = self.restaurants) => {
  * Add The service worker .
  */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-        .then((reg) => {
-            // registration worked
-            if (reg.installing) {
-                console.log('Service worker installing');
-            } else if (reg.waiting) {
-                console.log('Service worker installed');
-            } else if (reg.active) {
-                console.log('Service worker active');
-            }
-            console.log('Registration succeeded. Scope is ' + reg.scope);
-        }).catch((error) => {
-            // registration failed
-            console.log('Registration failed with ' + error);
-        });
+  navigator.serviceWorker.register('./sw.js', {scope: ''})
+  .then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
 }
