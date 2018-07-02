dde12ceb811d437046549f5ae18c990e8c66b2eb
SaraMagdy94 <30289008+SaraMagdy94@users.noreply.github.com>
2018-07-02 03:21:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -51,13 +51,6 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-});
 
 
 self.addEventListener('fetch', function(event) {
